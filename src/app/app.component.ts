8c717ba0c6bfbe9ae862d408036daa22ee2ad1b4
AlexandraZ <aleksandra12mail@mail.ru>
2024-01-22 23:17:18 +0800

    fourth commit№1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -117,8 +117,11 @@ constructor(private fb: FormBuilder){
 
 }
 
-  scrollTo(target:HTMLElement){
+  scrollTo(target: HTMLElement, burger?: any){
     target.scrollIntoView({ behavior: "smooth" });
+    if (burger) {
+  this.form.patchValue({order: burger.title + ' (' + burger.price + ' ' + this.currensy + ')'});
+}
   }
 
   confirmOrder(){
